279db25296a50d529a9d03533a1d8148f0c2c597
Aleksandra Kulbaka <awkulbaka1@sheffield.ac.uk>
2020-09-06 18:14:39 +0200

    Modified the result page, added feedback form and link to logo

--- app/main.py
+++ app/main.py
@@ -5,30 +5,23 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 def get_rules():
-  with open('app/static/restriction.csv') as csv_file:
+  with open('app/static/countries.csv') as csv_file:
     data = csv.reader(csv_file, delimiter=',')
     first_line = True
     rules = []
     for row in data:
       if not first_line:
         rules.append({
+          "name": str(row[0]).replace(" ", ""),
           "country": row[0],
-          "risk": row[1],
-          "flight": row[2],
-          "quarantine": row[3]
+          "flight": row[1],
+          "quarantine": row[2],
+          "additions": row[3]
         })
       else:
         first_line = False
   return rules
 
-def get_description():
-  description = {
-  "country": 'Country: ',
-  "risk": 'Risk of getting infected: ',
-  "flight": 'Can you travel there: ',
-  "quarantine": 'Do you have to be in quarantine once you arrive: '
-  }
-  return description
 
 @app.route("/")
 def index():
@@ -45,15 +38,25 @@ def submit():
   elif request.method == "POST":
     country = request.form.get('country_to')
     for rule in rules:
-      if rule['country'] == country:
+      if rule['name'] == country:
+        country = rule['country']
+        if rule['quarantine'] == '0':
+          quarantine = 'You do not have to be in quarantine once you arrive.'
+        else:
+          quarantine = 'Once you arrive, you need to be in quarantine for: ' +  rule['quarantine']
+
+        if rule['flight'].strip().lower() == 'yes':
+          flight = 'You can travel there ' + rule['additions']
+        else:
+          flight = 'No, you are not allowed to travel there.'
+          quarantine = ''
+
         response = {
           "country": country,
-          "risk": rule['risk'],
-          "flight": rule['flight'],
-          "quarantine": rule['quarantine']
+          "flight": flight,
+          "quarantine": quarantine
         }
-        break 
-      
-    description = get_description()       
-  return render_template("index.html", rules=rules, response=response, description=description)
+        break   
+    
+  return render_template("restrictions.html", response=response)
 
